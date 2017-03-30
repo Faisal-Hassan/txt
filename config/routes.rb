4a1381fae9e299cc676bb640d0cf84234dc84883
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-30 02:03:07 +0000

    Generate integration tests for site layout and add rails controller testing gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'home' => 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'help' => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
